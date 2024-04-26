d02fed238a39ddd0ef13568879076309214477f8
Shital Mainali <36978486+spiderb59@users.noreply.github.com>
2024-04-26 13:25:30 +0000

    created product detail page

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,8 +1,26 @@
 <template>
-    <h1>This is a detail page</h1>
+  <h1>{{ product.name }}</h1>
+    <div class= "img-wrap">
+     <img :src="product.imageName"/>
+    </div>
+    <div class="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 class="price">
+        {{ product.price }}
+      </h3>
+      <button class="add-to-cart">Add to cart</button>
+    </div>
 </template>
 <script>
+import { products } from '../temp-data';
+
 export default {
   name: 'ProductDetailPage',
+  data(){
+  return{
+    product: products.find(product=> product.id == this.$route.params.productId),
+  }
+}
 }
 </script>
+

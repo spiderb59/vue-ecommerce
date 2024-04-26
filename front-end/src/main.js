fbd7c1473bf7e9333fe8c7c384852f264ab782e9
Shital Mainali <36978486+spiderb59@users.noreply.github.com>
2024-04-26 09:03:37 +0000

    added routes for the pages

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as VueRouter from 'vue-router'
+import ShoppingCartPage from './pages/ShoppingCartPage.vue';
+import ProductPage from './pages/ProductPage.vue';
+import ProductDetailPage from './pages/ProductDetailPage.vue';
 
-createApp(App).mount('#app')
+
+createApp(App)
+.use(VueRouter.createRouter({
+    history:VueRouter.createWebHistory(process.env.Base_URL),
+    routes:
+    [
+        {
+            path: '/cart',
+            component: ShoppingCartPage, 
+        }, 
+        {
+            path: '/products',
+            component: ProductPage,  
+        },
+        {
+            path: '/products/:productId',
+            component: ProductDetailPage, 
+        }
+    ]
+
+}))
+.mount('#app')

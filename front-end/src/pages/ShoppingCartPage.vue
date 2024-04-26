9ac0cdf8dcdfbc36df1452899a5a3250ea57ba4a
Shital Mainali <36978486+spiderb59@users.noreply.github.com>
2024-04-26 13:36:33 +0000

    cart page layout created

--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -1,8 +1,32 @@
 <template>
-    <h1>This is a cart page</h1>
+    <h1>My Cart</h1>
+    <div v-if="cartItems.length>0">
+      <div 
+        v-for="product in cartItems"
+        class="product-container"
+        :key= "product.id"
+      >
+      <img class="product-image" :src="product.imageName" />
+      <div class="details-wrap">
+        <h3>{{ product.name}}</h3>
+        <p>{{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove from cart</button>
+    </div>
+  </div>
+  <div v-if="cartItems.length === 0">
+    No items in
+  </div>
+   
 </template>
 <script>
+import { cartItems } from '../temp-data';
 export default {
   name: 'ShoppingCartPage',
+  data(){
+    return{
+      cartItems,
+    }
+  }
 }
 </script>

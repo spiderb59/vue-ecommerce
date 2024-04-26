3abc58c3402446328d21cf403eb20d561bf3ae87
Shital Mainali <36978486+spiderb59@users.noreply.github.com>
2024-04-26 08:56:43 +0000

    Pages Directory Created

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hello From Vue"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
---
+++ front-end/src/pages/ProductPage.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>This is a product page</h1>
+</template>
+<script>
+export default {
+  name: 'ProductPage',
+}
+</script>
